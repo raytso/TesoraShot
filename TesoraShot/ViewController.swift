ac0d3abd7a574d68067607d4cd91ea6304915504
Ray Tso <raytso@users.noreply.github.com>
2019-02-18 21:38:26 -0800

    initial commit

---
+++ TesoraShot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesoraShot
+//
+//  Created by Ray Tso on 2019/2/18.
+//  Copyright © 2019 Ray Tso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
