08f44a5a787611a71f108b86bc9658c3cf4d81c3
Ray Tso <raytso@users.noreply.github.com>
2019-02-18 22:05:42 -0800

    setup enviroment

---
+++ TesoraShot/Solutions/SolutionsProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  SolutionsProtocol.swift
+//  TesoraShot
+//
+//  Created by Ray Tso on 2019/2/18.
+//  Copyright © 2019 Ray Tso. All rights reserved.
+//
+
+import Foundation
+
+protocol SolutionsProtocol {
+    
+}
